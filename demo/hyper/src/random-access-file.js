6ad99b803c8c26b0f01b46e3ab8e064e4bec5636
Irakli Gozalishvili <rfobic@gmail.com>
2018-06-20 13:41:03 -0700

    Implement write queue to avoid races  on parallel writes.

--- demo/hyper/src/random-access-file.js
+++ demo/hyper/src/random-access-file.js
@@ -3,8 +3,6 @@
 const RandomAccess = require("random-access-storage")
 const { Buffer } = require("Buffer")
 
-const MAX_SIZE = (1 << 30) * 2 - (1 << 12) - 1
-
 class RandomAccessFile extends RandomAccess {
   constructor(volume, name, options = {}) {
     super()
@@ -13,6 +11,8 @@ class RandomAccessFile extends RandomAccess {
     this.volume = volume
     this.url = `${volume.url}${name}`
     this.file = null
+    this.writeQueue = []
+    this.isIdle = true
   }
   static async mount(url = null) {
     const volume = await browser.FileSystem.mount({
@@ -38,7 +38,7 @@ class RandomAccessFile extends RandomAccess {
           ? await browser.File.read(file, { position: 0, size: position })
           : null
 
-      self.file = await browser.FileSystem.open(self.fileURL, {
+      self.file = await browser.FileSystem.open(self.url, {
         truncate: true,
         read: true,
         write: true
@@ -49,73 +49,80 @@ class RandomAccessFile extends RandomAccess {
       }
     }
   }
+  static async read(file, buffer, position, size) {
+    const content = await browser.File.read(file, {
+      position,
+      size
+    })
+    Buffer.from(content).copy(buffer)
+    return buffer
+  }
+  static async write(file, buffer, position, size) {
+    const wrote = await browser.File.write(file, buffer, {
+      position,
+      size
+    })
+    // await browser.File.flush(file)
+
+    return wrote
+  }
+  static async resumeWrites(self) {
+    const { writeQueue } = self
+    self.isIdle = false
+    let index = 0
+    while (index < writeQueue.length) {
+      const request = writeQueue[index++]
+      const { offset, size, data } = request
+      try {
+        await RandomAccessFile.write(self.file, data.buffer, offset, size)
+        request.callback(null)
+      } catch (error) {
+        request.callback(error)
+      }
+    }
+    writeQueue.length = 0
+    self.isIdle = true
+  }
   _open(request) {
-    console.log("_open", this.url, request)
     RandomAccessFile.open(this, { read: true, write: true, create: true })
       .then(self => request.callback(null, self))
       .catch(error => request.callback(error))
   }
   _openReadonly(request) {
-    console.log("_openReadonly", this.url, request)
     RandomAccessFile.open(this, { read: true })
       .then(self => request.callback(null, self))
       .catch(error => request.callback(error))
   }
   _write(request) {
-    const { offset, size, data } = request
-    console.log("_write", this.url, offset, size, data, request)
-    browser.File.write(this.file, data.buffer, {
-      position: offset,
-      size
-    })
-      .then(() => request.callback(null))
-      .catch(error => request.callback)
-  }
-  static async read(file, buffer, position, size) {
-    const content = await browser.File.read(file, {
-      position: position,
-      size: size //MAX_SIZE < size ? undefined : size
-    })
-    Buffer.from(content).copy(buffer)
-    return buffer
-
-    // if (data.byteLength < options.size) {
-    //   const result = new Uint8Array(options.size)
-    //   result.set(data)
-    //   return result
-    // } else {
-    //   return data
-    // }
+    this.writeQueue.push(request)
+    if (this.isIdle) {
+      RandomAccessFile.resumeWrites(this)
+    }
   }
   _read(request) {
     const { offset, size } = request
-    console.log(`_read`, this.url, offset, size, request)
     const buffer = request.data || Buffer.allocUnsafe(size)
     RandomAccessFile.read(this.file, buffer, offset, size)
       .then(data => request.callback(null, data))
       .catch(error => request.callback(error))
   }
   _del(request) {
-    console.log("_del", this.url, request)
     RandomAccessFile.delete(this, request.offset, request.size)
       .then(() => request.callback(null))
       .catch(error => request.callback(null))
   }
   _stat(request) {
-    console.log("_stat", this.url, request)
     browser.File.stat(this.file)
       .then(stat => request.callback(null, stat))
       .catch(error => request.callback(error))
   }
   _close(request) {
-    console.log("_close", this.url, request)
     browser.File.close(this.file)
       .then(() => request.callback((this.file = null)))
       .catch(error => request.callback(error))
   }
   _destroy(request) {
-    console.log("_destroy", this.url, request)
-    browser.FileSystem.removeFile(this.fileURL, { ignoreAbsent: true })
+    browser.FileSystem.removeFile(this.url, { ignoreAbsent: true })
       .then(() => request.callback(null))
       .catch(error => request.callback(error))
   }
